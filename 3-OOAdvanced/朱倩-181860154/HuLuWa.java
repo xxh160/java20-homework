c70b92d0e8ff569cc239914b4c7c79df571160ea
Qian Zhu <infinite0124@163.com>
2020-09-27 00:31:12 +0800

    temp submit

---
+++ 3-OOAdvanced/朱倩-181860154/HuLuWa.java
@@ -0,0 +1,32 @@
+
+
+public class HuLuWa {
+    private String name;
+    private int rank;
+  
+    HuLuWa(String name, int rank) {
+      this.name = name;
+      this.rank = rank;
+    }
+  
+    public boolean youngerThan(HuLuWa h) {
+      return this.rank > h.rank;
+    }
+  
+    public void exchangeWith(HuLuWa h) {
+      String tempname = h.name;
+      h.name = this.name;
+      this.name = tempname;
+      int temprank = h.rank;
+      h.rank = this.rank;
+      this.rank = temprank;
+    }
+  
+    public int getRank() {
+      return rank;
+    }
+  
+    public void tellName() {
+      System.out.print(name + " ");
+    }
+  }
