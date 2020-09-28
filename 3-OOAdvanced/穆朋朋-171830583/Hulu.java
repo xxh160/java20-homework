e6149f0f2912fa7444e6f9d364279ff9445c2cf0
stnjumu <1282807292@qq.com>
2020-09-28 15:10:36 +0800

    hulu finish

---
+++ 3-OOAdvanced/穆朋朋-171830583/Hulu.java
@@ -0,0 +1,14 @@
+public class Hulu {
+    private int color;  // 颜色；0-6分别代表葫芦娃的大娃到七娃；
+    
+    Hulu() {
+        color=0;
+    }
+
+    Hulu(int color) {
+        this.color=color;
+    }
+    int getColor() {
+        return color;
+    }
+}
