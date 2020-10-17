9760e7ffe80438878c990abc0562840dd64ad4f2
oxygen-hunter <924273413@qq.com>
2020-10-17 09:05:39 +0800

    改进了类加载器的接口

--- 5-Class/李骁-171860699/Base64ClassLoader.java
+++ 5-Class/李骁-171860699/Base64ClassLoader.java
@@ -1,22 +1,9 @@
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Base64;
-import java.io.FileOutputStream;
 
 public class Base64ClassLoader extends ClassLoader {
 
-    private String base64;
-    private byte[] buffer;
-
-    public Base64ClassLoader(String base64) throws Exception {
-        this.base64 = base64;
-        buffer = Base64.getDecoder().decode(base64);
-    }
-
-    @Override
-    public Class findClass(String name) throws ClassNotFoundException {
+    public Class<?> findClass(String name, String base64) {
+        byte[] buffer = Base64.getDecoder().decode(base64);
         return defineClass(name, buffer, 0, buffer.length);
     }
 }
