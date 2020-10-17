7100e89e1593145185c7a46357dcb86005c9f8f8
Hoyyyywolf <55869568+Hoyyyywolf@users.noreply.github.com>
2020-10-17 14:29:37 +0800

    modify “LoadClass.java” to create an instance of Monster

--- 5-Class/祝嘉龙-181180220/LoadClass.java
+++ 5-Class/祝嘉龙-181180220/LoadClass.java
@@ -7,8 +7,9 @@ public class LoadClass{
 		Base64ClassLoader bcl = new Base64ClassLoader();
 		try{
 			Class<?> ec = bcl.loadClass("EncrpytedClass");
-			Method[] methods = ec.getMethods();
-			Field[] fields = ec.getFields();
+			Method[] methods = ec.getDeclaredMethods();
+			Field[] fields = ec.getDeclaredFields();
+			Constructor[] ctors = ec.getDeclaredConstructors();
 			System.out.println("Class Name:");
 			System.out.println(ec.getName());
 			System.out.println("Field:");
@@ -19,6 +20,14 @@ public class LoadClass{
 			for(Method m : methods){
 				System.out.println(p.matcher(m.toString()).replaceAll(""));
 			}
+			System.out.println("Constructors:");
+			for(Constructor c : ctors){
+				System.out.println(p.matcher(c.toString()).replaceAll(""));
+			}
+			System.out.println("Instance:");
+			ctors[0].setAccessible(true);
+			Object obj = ctors[0].newInstance("Naga", 999, 999);
+			System.out.println(obj);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
