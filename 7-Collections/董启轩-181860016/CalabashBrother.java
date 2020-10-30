015d53786546297edfb58ad3f98267b0bc95f34c
董启轩 <dongqixuan@dongqixuandeMacBook-Air.local>
2020-10-30 20:24:49 +0800

    finish homework7

---
+++ 7-Collections/董启轩-181860016/CalabashBrother.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+enum Gender{
+    MALE,FEMALE,UNKNOWN
+}
+
+public class CalabashBrother implements Comparable<CalabashBrother>{
+    private static final String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int NAME_LENGTH=7;
+    private String name;
+    private Gender gender;
+    CalabashBrother(){
+        Random random=new Random();
+        StringBuffer stringBuffer=new StringBuffer();
+        for(int i=0;i<NAME_LENGTH;i++){
+            int num=random.nextInt(str.length());
+            stringBuffer.append(str.charAt(num));
+        }
+        name=stringBuffer.toString();
+        Random r=new Random();
+        int i=r.nextInt(2);
+        switch(i){
+            case 0:
+            gender=Gender.MALE;
+            break;
+            case 1:
+            gender=Gender.FEMALE;
+            break;
+            default:
+            gender=Gender.UNKNOWN;
+        }
+    }
+    CalabashBrother(String Name,Gender sex){
+        name=Name;
+        gender=sex;
+    }
+    public String getName(){
+        return name;
+    }
+    public boolean is_male(){
+        return gender.equals(Gender.MALE);
+    }
+    public boolean is_female(){
+        return gender.equals(Gender.FEMALE);
+    }
+    public void printInformation(){
+        System.out.println("Name:"+name+"  "+"Gender:"+gender);
+    }
+    public int compareTo(CalabashBrother bro){
+        String broname=bro.getName();
+        return name.compareTo(broname);
+    }
+}
---
+++ 7-Collections/董启轩-181860016/CalabashBrotherComparator.java
@@ -0,0 +1,31 @@
+import java.util.Comparator;
+import java.util.Random;
+enum Order{
+    POSITIVE,NEGATIVE,RANDOM
+}
+public  class CalabashBrotherComparator implements Comparator<CalabashBrother>{
+    public Order order;
+    CalabashBrotherComparator(){
+        order=Order.POSITIVE;
+    }
+    CalabashBrotherComparator(Order o){
+        order=o;
+    }
+    public void set_order(Order o){
+        order=o;
+    }
+    public int compare(CalabashBrother bro1,CalabashBrother bro2){
+        switch(order){
+            case POSITIVE:
+            return bro1.compareTo(bro2);
+            case NEGATIVE:
+            return -bro1.compareTo(bro2);
+            default:
+            Random r=new Random();
+            int randnum1=r.nextInt();
+            int randnum2=r.nextInt();
+            return randnum1-randnum2;
+        }
+    }
+
+}
