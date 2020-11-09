a87d326d4330eba9412edb0919d71f38f9334c72
sevenxqq <fantastic772256@163.com>
2020-11-09 19:43:19 +0800

    super T

--- 8-Generics/徐佳美-181860117/Sortset.java
+++ 8-Generics/徐佳美-181860117/Sortset.java
@@ -1,7 +1,7 @@
 
 import java.util.Comparator;
 
-abstract class Sortset<T extends Comparable<T>> implements Comparator<T> {
+abstract class Sortset<T extends Comparable<? super T>> implements Comparator<T> {
     @Override
     public abstract int compare(T a,T b);
 }
