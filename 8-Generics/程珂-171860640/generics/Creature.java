08b418637768175ae7e6c2afe704f675853eb048
ChengKer <564157441@qq.com>
2020-11-09 19:27:01 +0800

    Submit hw8

---
+++ 8-Generics/程珂-171860640/generics/Creature.java
@@ -0,0 +1,15 @@
+package generics;
+
+import java.util.Comparator;
+
+enum Gender{MALE,FEMALE}
+
+abstract class CreatureComparator<T extends Creature> implements Comparator<T> {
+     abstract public int compare(T creature1,T creature2);
+}
+
+public class Creature {
+    //后续可能会添加生命值，法术值，攻击力等元素，当前还未涉及
+     protected Gender g;
+     protected String name;
+}
