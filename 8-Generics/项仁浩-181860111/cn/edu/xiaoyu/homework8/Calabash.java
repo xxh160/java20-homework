8bb0280c79c90edaa3e419a84189c3808641b46b
项仁浩 <2579348964@qq.com>
2020-11-10 00:21:30 +0800

    8th HW

---
+++ 8-Generics/项仁浩-181860111/cn/edu/xiaoyu/homework8/Calabash.java
@@ -0,0 +1,56 @@
+package cn.edu.xiaoyu.homework8;
+
+public class Calabash implements Comparable<Calabash>{
+    Calabash(){}
+    String name;
+    Gender gender;
+    Calabash(String name,Gender gender){
+        this.name=name;
+        this.gender=gender;
+    }
+
+    @Override
+    public int compareTo(Calabash o) {
+        return name.compareTo(o.name);
+    }
+
+    public void change(Calabash c){
+        name = c.name;
+        gender = c.gender;
+    }
+
+    public String act(){
+        return "I am just a ordinary Calabash!";
+    }
+}
+
+class CalabashFire extends Calabash{
+    CalabashFire(){
+        super();
+    }
+    CalabashFire(String name,Gender gender){
+        super(name,gender);
+    }
+
+    @Override
+    public String act() {
+        return "I can spray fire!";
+    }
+}
+
+class CalabashWater extends Calabash{
+    CalabashWater(){
+        super();
+    }
+    CalabashWater(String name,Gender gender){
+        super(name,gender);
+    }
+
+    @Override
+    public String act(){
+       return "I can spit Water!";
+    }
+}
+
+
+
