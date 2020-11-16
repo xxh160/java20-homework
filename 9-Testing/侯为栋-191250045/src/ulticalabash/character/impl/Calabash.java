fbb1d73c8ec2956fac27cf14494477f9afc93e64
xxh160 <55646006+xxh160@users.noreply.github.com>
2020-11-16 23:56:56 +0800

    hw9

---
+++ 9-Testing/侯为栋-191250045/src/ulticalabash/character/impl/Calabash.java
@@ -0,0 +1,17 @@
+package ulticalabash.character.impl;
+
+import ulticalabash.character.Creature;
+import ulticalabash.util.Gender;
+
+public class Calabash extends Creature {
+
+    public Calabash() {
+        super();
+    }
+
+    public Calabash(String name, Gender sex) {
+        super(name, sex);
+    }
+
+
+}
