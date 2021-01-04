3e0424425f54e8fc303fed54f64054da5eec506f
LDawns <250858445@qq.com>
2021-01-04 21:14:56 +0800

    deleted

--- X-FinalProject/李思勉-171860524/UHLW/src/main/java/cn/edu/nju/role/Action.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.nju.role;
-import cn.edu.nju.field.BattleField;
-
-public interface Action {
-    void walkTo(BattleField<Creature> battleField, int row, int column);
-    void tellName();
-}
